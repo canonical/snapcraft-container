305da930e47a938565f7c822e2beff2da2622aaa
Hook25 <massimiliano.girardi@canonical.com>
2026-05-26 12:27:24 +0200

    Remove exit code bodge
    Minor: remove dead code (entrypoint no longer used)

--- download-snap.sh
+++ download-snap.sh
@@ -10,15 +10,20 @@ ARCH="$(cat /tmp/arch)"
 
 API_URL="https://api.snapcraft.io/api/v1/snaps/details/${SNAP_NAME}?channel=${CHANNEL}"
 
-DOWNLOAD_URL=$(curl -s -H 'X-Ubuntu-Series: 16' -H "X-Ubuntu-Architecture: ${ARCH}" \
+DOWNLOAD_URL=$(curl --retry 5 -s -H 'X-Ubuntu-Series: 16' -H "X-Ubuntu-Architecture: ${ARCH}" \
     "$API_URL" | jq '.download_url' -r)
 
 echo "Downloading ${SNAP_NAME} (channel: ${CHANNEL}, arch: ${ARCH})..."
-curl -L "$DOWNLOAD_URL" --output /tmp/"${SNAP_NAME}.snap"
+curl --retry 5 -L "$DOWNLOAD_URL" --output /tmp/"${SNAP_NAME}.snap"
 
 mkdir -p "/snap/${SNAP_NAME}"
-# Exit code 2 from unsquashfs is typically from failing to create device files, which is harmless
-unsquashfs -d "/snap/${SNAP_NAME}/current" /tmp/"${SNAP_NAME}.snap" || true
+# proc fails to extract but we don't need it so its fine
+
+unsquashfs -l /tmp/"${SNAP_NAME}.snap" | \
+  grep -v '^squashfs-root/proc' | \
+  sed 's/^squashfs-root\///' | \
+  grep -v '^squashfs-root$' > /tmp/${SNAP_NAME}keep_list.txt
+unsquashfs -d "/snap/${SNAP_NAME}/current" -ef /tmp/${SNAP_NAME}keep_list.txt  /tmp/"${SNAP_NAME}.snap"
 
 rm -f "${SNAP_NAME}.snap"
 
